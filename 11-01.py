19b810a66b93a6a3f24b2a4904e0f8838f6d81aa
hchuzhong <hchuzhong@163.com>
2024-06-19 21:29:02 +0800

    chapter 11

---
+++ 11-01.py
@@ -0,0 +1,13 @@
+import re
+
+hand = open('mbox-short.txt')
+for line in hand:
+    line = line.rstrip()
+    if re.search('^From:', line):
+        print(line)
+
+x = 'My 2 favorite numbers are 19 and 42'
+y = re.findall('[0-9]+', x)
+print(y)
+y = re.findall('[AEIOU]+', x)
+print(y)
