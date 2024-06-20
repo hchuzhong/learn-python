f6cc294d0e4db752ce1c1338afa7b098e8dd5824
hchuzhong <hchuzhong@163.com>
2024-06-20 17:57:11 +0800

    chapter 12

---
+++ 12-02.py
@@ -0,0 +1,6 @@
+# http://data.pr4e.org/romeo.txt
+import urllib.request, urllib.parse, urllib.error
+
+fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+for line in fhand:
+    print(line.decode().strip())
